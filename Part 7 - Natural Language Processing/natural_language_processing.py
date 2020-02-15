180fcab9e67663905ffcf6d66902aaf436d13605
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2020-02-15 11:53:38 +0700

    🧪 Arguments experiment

--- Part 7 - Natural Language Processing/natural_language_processing.py
+++ Part 7 - Natural Language Processing/natural_language_processing.py
@@ -14,7 +14,9 @@ import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-corpus = []
+
+corpus = list()
+
 for i in range(0, 1000):
     review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
     review = review.lower()
@@ -23,16 +25,19 @@ for i in range(0, 1000):
     review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
     review = ' '.join(review)
     corpus.append(review)
+del i, ps
 
 # Creating the Bag of Words model
 from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer(max_features = 1500)
-X = cv.fit_transform(corpus).toarray()
+count_vectorizer = CountVectorizer(max_features = 900)
+X = count_vectorizer.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
 
+del count_vectorizer
+
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.15, random_state = 0)
 
 # Fitting Naive Bayes to the Training set
 from sklearn.naive_bayes import GaussianNB
@@ -44,4 +49,5 @@ y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+confusion_matrix = confusion_matrix(y_test, y_pred)
+accuracy = (confusion_matrix[0, 0] + confusion_matrix[1, 1]) / sum(sum(confusion_matrix))
