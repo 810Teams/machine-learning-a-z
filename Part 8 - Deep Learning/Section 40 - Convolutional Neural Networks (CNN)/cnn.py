16439718aad3eb397f245f1c00ce856e81304cc6
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2020-02-15 12:01:15 +0700

    🚚 Files Throw-in from Deep Learning A-Z

---
+++ Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/cnn.py
@@ -0,0 +1,87 @@
+# Convolutional Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# pip install tensorflow
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 0 - Stop CNN from terminating itself
+# ERROR MESSAGE
+# OMP: Error #15: Initializing libiomp5.dylib, but found libiomp5.dylib already initialized.
+# OMP: Hint This means that multiple copies of the OpenMP runtime have been linked into the program.
+#      That is dangerous, since it can degrade performance or cause incorrect results.
+#      The best thing to do is to ensure that only a single OpenMP runtime is linked into the process,
+#      e.g. by avoiding static linking of the OpenMP runtime in any library.
+#      As an unsafe, unsupported, undocumented workaround you can set the environment variable
+#      KMP_DUPLICATE_LIB_OK=TRUE to allow the program to continue to execute,
+#      but that may cause crashes or silently produce incorrect results.
+#      For more information, please see http://www.intel.com/software/products/support/.
+# Abort trap: 6
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Part 2 - Fitting the CNN to the images
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch = 8000,
+                         epochs = 25,
+                         validation_data = test_set,
+                         validation_steps = 2000)
+
+# classifier.save('model.h5')
+
